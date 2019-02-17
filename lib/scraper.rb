f766a7f96a4afc5bc1841533dea39309626ead4c
Maximo Bautista <max.bautista97@gmail.com>
2019-02-17 21:32:49 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -27,12 +27,12 @@ class Scraper
   
   def make_courses
     self.get_courses.each do |post|
-      if 
-      course = Course.new
-      course.title = post.css("h2").text
-      course.schedule = post.css(".date").text
-      course.description = post.css("p").text
-      
+      if post.css("h2").text != nil
+        course = Course.new
+        course.title = post.css("h2").text
+        course.schedule = post.css(".date").text
+        course.description = post.css("p").text
+      end
     end
   end
   
